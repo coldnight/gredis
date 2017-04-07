009a1c023d3c7a1eca816bd2e566f990fb7c4be4
wanghui <wh_linux@126.com>
2017-04-07 11:39:36 +0800

    Add tox & coverage measurement

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#
-#   Author  :   cold
-#   E-mail  :   wh_linux@126.com
-#   Date    :   15/04/18 16:46:57
-#   Desc    :
-#
-from __future__ import absolute_import, print_function, division, with_statement
+from __future__ import unicode_literals, print_function, division
 
 from gredis import version
 
@@ -15,14 +9,15 @@ from setuptools import setup
 setup(
     name="gredis",
     version=version,
-    packages = ["gredis"],
-    package_data = {
+    packages=["gredis"],
+    package_data={
     },
     author="cold",
     author_email="wh_linux@126.com",
     url="https://github.com/coldnight/gredis",
     license="http://www.apache.org/licenses/LICENSE-2.0",
-    description="gRedis is an asynchronous client library of Redis written with Tornado coroutine.",
+    description=("gRedis is an asynchronous client library of Redis written "
+                 "with Tornado coroutine."),
     long_description=open("README.rst").read(),
     install_requires=["tornado>=4.0", "redis"],
     classifiers=[
